2b9369238d5528741df8739ecd9afed50d0752ff
MaGlou <maelmenn28@gmail.com>
2025-05-21 15:53:43 +0200

    RP3

--- encrypt_password.php
+++ encrypt_password.php
@@ -1,19 +1,18 @@
 <?php
-// Paramètres de chiffrement
 $key = "ma_cle_secrete_32_bytes";  // La clé (doit être de 32 caractères pour AES-256)
 $iv = "1234567890123456";  // L'IV (initialisation vector), doit être de 16 caractères
 
-$password_plain = "Azerty.1@";  // Ton mot de passe en clair
+$password_plain = "Azerty.1@";  //mdp en clair
 
 // Chiffrement du mot de passe
 $encrypted_password = openssl_encrypt($password_plain, 'AES-256-CBC', $key, 0, $iv);
 
-// Vérifier si le mot de passe a été correctement chiffré
+// Vérif mot de passe chiffré
 if ($encrypted_password === false) {
     echo "Erreur de chiffrement.\n";
     exit;
 }
 
-// Afficher le mot de passe chiffré
+// Affichage du mot de passe chiffé
 echo "Mot de passe chiffré : " . $encrypted_password . PHP_EOL;
 ?>
